72fa6634957bd01d0d23633c21fefa15a26b93bc
Jinping Wu <jinping.wu@verisilicon.com>
2023-10-16 13:56:09 +0800

    fix thomas-riscv64

--- hw/riscv/thomas_riscv64.c
+++ hw/riscv/thomas_riscv64.c
@@ -81,8 +81,8 @@ static const MemMapEntry base_memmap[] = {
     [THOMAS_RISCV64_PLIC] =         {  0xc000000,  0x4000000 },
     [THOMAS_RISCV64_UART0] =        { 0x10000000,     0x1000 },
     [THOMAS_RISCV64_FLASH] =        { 0x20000000, 0x02000000 },
-    [THOMAS_RISCV64_TEST_DEVICE] =  { 0x60000000,     0x1000 },
-    [THOMAS_RISCV64_SRAM] =         { 0x80000000, 0x08000000 },
+    [THOMAS_RISCV64_TEST_DEVICE] =  { 0x30000000,     0x1000 },
+    [THOMAS_RISCV64_SRAM] =         { 0x40000000, 0x08000000 },
 };
 
 struct THOMASRISCV64MachineClass {
@@ -112,7 +112,7 @@ static void thomas_riscv64_common_init(MachineState *machine)
     object_initialize_child(OBJECT(mms), "cpus", &mms->cpus, TYPE_RISCV_HART_ARRAY);
     object_property_set_int(OBJECT(&mms->cpus), "num-harts", 1,
                             &error_abort);
-    object_property_set_int(OBJECT(&mms->cpus), "resetvec", base_memmap[THOMAS_RISCV64_SRAM].base, &error_abort);
+    object_property_set_int(OBJECT(&mms->cpus), "resetvec", base_memmap[THOMAS_RISCV64_FLASH].base, &error_abort);
 
     /* Realize */
     object_property_set_str(OBJECT(&mms->cpus), "cpu-type", ms->cpu_type,
